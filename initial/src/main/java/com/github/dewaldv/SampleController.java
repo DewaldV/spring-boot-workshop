9c32d84f79b609d13c5bb99a9bddfe7c7457c0d5
DewaldV <dewald.viljoen@gmail.com>
2014-07-11 07:46:19 +0200

    Fix up initial project.

--- initial/src/main/java/com/github/dewaldv/SampleController.java
+++ initial/src/main/java/com/github/dewaldv/SampleController.java
@@ -1,10 +1,15 @@
+package com.github.dewaldv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @EnableAutoConfiguration
+@ComponentScan
 @RestController
 public class SampleController {
 
