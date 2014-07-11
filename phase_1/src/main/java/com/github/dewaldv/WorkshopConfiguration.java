351ddd6e39667589dd5fca836967ad61c7c44e08
DewaldV <dewald.viljoen@gmail.com>
2014-07-11 07:58:13 +0200

    Add Phase 1 with broken up components for better clarity.

---
+++ phase_1/src/main/java/com/github/dewaldv/WorkshopConfiguration.java
@@ -0,0 +1,11 @@
+package com.github.dewaldv;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class WorkshopConfiguration {
+}
